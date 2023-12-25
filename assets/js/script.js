f033ef6dd17eb9e6453a1ac0770c4d20b033d270
fariz <farizhazmi5@gmail.com>
2023-12-25 13:08:36 +0700

    Sukses

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,16 @@ const wrapperNav = document.querySelector(".wrapper-nav");
 humburger.addEventListener("click", () => {
     wrapperNav.classList.toggle("active");
 });
+
+function openModal(src) {
+    var modal = document.getElementById("myModal");
+    var modalImg = document.getElementById("modalImg");
+
+    modal.style.display = "block";
+    modalImg.src = src;
+}
+
+function closeModal() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "none";
+}
